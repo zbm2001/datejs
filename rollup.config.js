971dab79adf8c8eded98a0361c9089a89ff7a0a6
ZhouBaiMin <zbm2001@aliyun.com>
2016-08-24 03:01:17 +0800

    rollup test modified

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,18 @@
+import buble from 'rollup-plugin-buble';
+
+var external = Object.keys( require( './package.json' ).devDependencies );
+
 export default { // 项目下直接命令 rollup -c
   entry: 'src/index.js',
+  //plugins: [ buble() ],
+  //external: external,
+  //targets: [
+  //  { dest: 'bundle.cjs.js', format: 'cjs' },
+  //  { dest: 'bundle.es.js', format: 'es' }
+  //  { dest: 'bundle.umd.js', format: 'umd', moduleName: 'Date' }
+  //]
   format: 'umd', // cjs amd es6 umd iife
-  dest: './bundle.js', // 输出文件
+  dest: 'bundle.js', // 输出文件
   moduleName: 'Date', // umd 或 iife 模式下，若入口文件含 export，必须加上该属性
   sourceMap: false   // 调试编译
 }
